71fdf903b3925bf9bf76f96a10ebfd510ee043e1
VSacr1 <vjsacre@gmail.com>
2022-10-14 11:23:30 +0100

    Added the delete functionality

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,5 @@
 # This is where your CREATE, READ, UPDATE AND DELETE functionality is going to go. 
+from asyncio import Task
 from flask import render_template, url_for, redirect, request 
 from application import app, db 
 from application.models import Todos, Lists
@@ -13,18 +14,34 @@ def index():
 #CREATE 
 @app.route('/add', methods=['POST','GET'])
 def add():
+    # This points to TodoForm
     form = TodoForm()
+    # Checks that we have clicked the submit button
     if form.validate_on_submit():
+        # the variable tasks becomes what is put on the form 
+        # todos becomes what we are going to be adding to the database
         todos = Todos(
             tasks = form.tasks.data
         )
-       
+        # This performs the add to database
         db.session.add(todos)
-        print("This is to test the submit")
+        # This commits those changes
         db.session.commit()
+        # This one redirects to the index functions url
         return redirect(url_for('index'))
+    # Otherwise return the template of add.html
     return render_template('add.html', title="Add a new Task", form=form)
 
 #UPDATE 
 
 #DELETE
+@app.route('/delete/<int:tid>')
+def delete(tid):
+    # Collecting the task we want to delete based on its id
+    tasks = Todos.query.get(tid)
+    # deleting this item from the database
+    db.session.delete(tasks)
+    # committing this change
+    db.session.commit()
+    # returning the url in the index function. 
+    return redirect(url_for('index'))
