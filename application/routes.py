49e2a7eb1569628a8499003d5365c3b2b00c7fb5
VSacr1 <vjsacre@gmail.com>
2022-10-14 11:43:36 +0100

    Completed update

--- application/routes.py
+++ application/routes.py
@@ -6,12 +6,14 @@ from application.models import Todos, Lists
 from application.forms import TodoForm, ListForm
 
 #READ 
+#Location of this functionality: ip_address:5000/
 @app.route('/', methods=['POST', 'GET'])
 def index():
     todos = Todos.query.all()
     return render_template('index.html', title="To do List", todos=todos)
 
 #CREATE 
+#Location of this functionality: ip_address:5000/add
 @app.route('/add', methods=['POST','GET'])
 def add():
     # This points to TodoForm
@@ -33,8 +35,30 @@ def add():
     return render_template('add.html', title="Add a new Task", form=form)
 
 #UPDATE 
+@app.route('/update/<int:tid>', methods=['GET', 'POST'])
+def update(tid):
+    form = TodoForm()
+    # Get one tasks from the specified ID
+    tasks = Todos.query.get(tid)
+    # POST method
+    # If the user clicks submit
+    if form.validate_on_submit():
+        # What is put in the form gets ammended to the database
+        tasks.tasks = form.tasks.data
+        # Commit the changes
+        db.session.commit()
+        # Redirect to the url for index function 
+        return redirect(url_for('index'))
+    # Else if the request method is a GET
+    elif request.method == 'GET':
+        # Update the form with whats in the database
+        form.tasks.data = tasks.tasks 
+    # If we go to the url return the template update.html
+    return render_template('update.html', title='Update you task', form=form)
+
 
 #DELETE
+#Location of this functionality: ip_address:5000/delete/1
 @app.route('/delete/<int:tid>')
 def delete(tid):
     # Collecting the task we want to delete based on its id
