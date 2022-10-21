78951ce36c62e04268061a32eb2680247bc32042
Victoria <32303351+VSacr1@users.noreply.github.com>
2022-10-21 10:21:45 +0100

    Update __init__.py

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__, template_folder="templates")
 
 # Sets up your database. We are using SQLAlchemy's sqlite. 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{os.getenv('ROOT_PASSWORD')}@mysql:3306/flask-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
