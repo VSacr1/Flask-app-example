b76931300243da7629693147076bb1b2e28872d5
Ubuntu <vsacre@vmjenkins.frcitqiblpxufm5ocvgedkyuba.ax.internal.cloudapp.net>
2022-10-21 09:11:29 +0000

    mysql connected

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__, template_folder="templates")
 
 # Sets up your database. We are using SQLAlchemy's sqlite. 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'Secrets'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 
